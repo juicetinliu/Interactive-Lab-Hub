a7921fba5f53e43813adc9f0697f481826d3d1cd
juicetinliu <37272242+juicetinliu@users.noreply.github.com>
2021-05-05 15:50:14 -0400

    Update sender.py

--- Lab 6/sender.py
+++ Lab 6/sender.py
@@ -4,21 +4,21 @@ import uuid
 # Every client needs a random ID
 client = mqtt.Client(str(uuid.uuid1()))
 # configure network encryption etc
-client.tls_set()
+# client.tls_set()
 # this is the username and pw we have setup for the class
-client.username_pw_set('idd', 'device@theFarm')
+# client.username_pw_set('idd', 'device@theFarm')
 
 #connect to the broker
 client.connect(
-    'farlab.infosci.cornell.edu',
-    port=8883)
+    'test.mosquitto.org',
+    port=8080)
 
 while True:
-	cmd = input('>> topic: IDD/')
+	cmd = input('>> topic: nani/')
 	if ' ' in cmd:
 		print('sorry white space is a no go for topics')
 	else:
-		topic = f"IDD/{cmd}"
+		topic = f"nani/{cmd}"
 		print(f"now writing to topic {topic}")
 		print("type new-topic to swich topics")
 		while True:
