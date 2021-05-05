8ccdf9c98539de5b374e69390dcd7ee9069ae4b1
juicetinliu <37272242+juicetinliu@users.noreply.github.com>
2021-05-05 16:26:36 -0400

    Update game-controller.py

--- Lab 6/game-controller.py
+++ Lab 6/game-controller.py
@@ -1,29 +1,94 @@
+import board
+import busio
+import adafruit_mpu6050
+import time
 import paho.mqtt.client as mqtt
 import uuid
+import signal
 
-# Every client needs a random ID
+import digitalio
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_rgb_display.st7789 as st7789
+
+
+# Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = None
+
+# Config for display baudrate (default max is 24mhz):
+BAUDRATE = 64000000
+
+backlight = digitalio.DigitalInOut(board.D22)
+backlight.switch_to_output()
+backlight.value = True
+buttonA = digitalio.DigitalInOut(board.D23)
+buttonB = digitalio.DigitalInOut(board.D24)
+buttonA.switch_to_input()
+buttonB.switch_to_input()
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+# Create the ST7789 display:
+disp = st7789.ST7789(
+    spi,
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+    width=135,
+    height=240,
+    x_offset=53,
+    y_offset=40,
+)
+
+height =  disp.height
+width = disp.width 
+image = Image.new("RGB", (width, height))
+draw = ImageDraw.Draw(image)
+
+
+i2c = busio.I2C(board.SCL, board.SDA)
+mpu = adafruit_mpu6050.MPU6050(i2c)
+
+topic = 'IDD/juicey/labyrinth'
+
+def on_connect(client, userdata, flags, rc):
+    print(f"connected with result code {rc}")
+    client.subscribe(topic)
+
 client = mqtt.Client(str(uuid.uuid1()))
-# configure network encryption etc
 client.tls_set()
-# this is the username and pw we have setup for the class
 client.username_pw_set('idd', 'device@theFarm')
+client.on_connect = on_connect
 
-#connect to the broker
 client.connect(
     'farlab.infosci.cornell.edu',
     port=8883)
 
+client.loop_start()
+
+# this lets us exit gracefully (close the connection to the broker)
+def handler(signum, frame):
+    print('exit gracefully')
+    client.loop_stop()
+    exit (0)
+
+# hen sigint happens, do the handler callback function
+signal.signal(signal.SIGINT, handler)
+
+# our main loop
 while True:
-	cmd = input('>> topic: IDD/')
-	if ' ' in cmd:
-		print('sorry white space is a no go for topics')
-	else:
-		topic = f"IDD/{cmd}"
-		print(f"now writing to topic {topic}")
-		print("type new-topic to swich topics")
-		while True:
-			val = input(">> message: ")
-			if val =='new-topic':
-				break
-			else:
-				client.publish(topic, val)
+    accel = mpu.acceleration
+    x, y, z = accel
+    
+    client.publish(topic, f"{x},{y},{z}")
+
+    # # if we press the button, send msg to cahnge everyones color
+    # if not buttonA.value:
+    #     client.publish(topic, f"{r},{g},{b}")
+    # draw.rectangle((0, height*0.5, width, height), fill=color[:3])
+    # disp.image(image)
+    time.sleep(.01)
+    
