2093d11c6728177bd3e413e3338d31685628a7e0
juicetinliu <37272242+juicetinliu@users.noreply.github.com>
2021-05-05 17:36:31 -0400

    Cleanup

--- Lab 6/game-controller.py
+++ Lab 6/game-controller.py
@@ -6,49 +6,6 @@ import paho.mqtt.client as mqtt
 import uuid
 import signal
 
-import digitalio
-from PIL import Image, ImageDraw, ImageFont
-import adafruit_rgb_display.st7789 as st7789
-
-
-# Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
-cs_pin = digitalio.DigitalInOut(board.CE0)
-dc_pin = digitalio.DigitalInOut(board.D25)
-reset_pin = None
-
-# Config for display baudrate (default max is 24mhz):
-BAUDRATE = 64000000
-
-backlight = digitalio.DigitalInOut(board.D22)
-backlight.switch_to_output()
-backlight.value = True
-buttonA = digitalio.DigitalInOut(board.D23)
-buttonB = digitalio.DigitalInOut(board.D24)
-buttonA.switch_to_input()
-buttonB.switch_to_input()
-
-# Setup SPI bus using hardware SPI:
-spi = board.SPI()
-
-# Create the ST7789 display:
-disp = st7789.ST7789(
-    spi,
-    cs=cs_pin,
-    dc=dc_pin,
-    rst=reset_pin,
-    baudrate=BAUDRATE,
-    width=135,
-    height=240,
-    x_offset=53,
-    y_offset=40,
-)
-
-height =  disp.height
-width = disp.width 
-image = Image.new("RGB", (width, height))
-draw = ImageDraw.Draw(image)
-
-
 i2c = busio.I2C(board.SCL, board.SDA)
 mpu = adafruit_mpu6050.MPU6050(i2c)
 
@@ -88,14 +45,8 @@ signal.signal(signal.SIGINT, handler)
 # our main loop
 while True:
     accel = mpu.acceleration
-    # x, y, z = accel
     
     client.publish(topic, f"{accel[p]}")
-
-    # # if we press the button, send msg to cahnge everyones color
-    # if not buttonA.value:
-    #     client.publish(topic, f"{r},{g},{b}")
-    # draw.rectangle((0, height*0.5, width, height), fill=color[:3])
-    # disp.image(image)
+    
     time.sleep(.01)
     
